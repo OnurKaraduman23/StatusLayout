01f4d971e3b8c84f752c48d1814282143c3beb6d
OnurKaraduman <onury.karaduman@gmail.com>
2024-07-24 19:18:17 +0300

    1.0.5

--- StatusLayoutLibrary/build.gradle.kts
+++ StatusLayoutLibrary/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.github.OnurKaraduman23"
                 artifactId = "statuslayout"
-                version = "1.0.4"
+                version = "1.0.5"
                 artifact("build/outputs/aar/StatusLayoutLibrary-release.aar")
             }
         }
