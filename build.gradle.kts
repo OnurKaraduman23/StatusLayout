2532397755674eb372aa102af9d19e3f91e8ea97
OnurKaraduman <onury.karaduman@gmail.com>
2024-07-24 18:59:07 +0300

    1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript{
+    repositories{
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
